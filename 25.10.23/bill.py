fdcfac6cf52984c1498be158166f9ddee1db88bf
Yanica3 <148972148+Yanica3@users.noreply.github.com>
2023-10-25 14:52:48 +0300

    Create bill.py

---
+++ 25.10.23/bill.py
@@ -0,0 +1,17 @@
+for i in range(0, 11):
+    if i == 0 or i == 10:
+        print('----------')
+    elif i == 7:
+        print('|--------|')
+    elif i == 2:
+        print('| Cinema |')
+    elif i == 4:
+        print('| Ticket |')
+    elif i == 5:
+        print('|Popcorn |')
+    elif i == 6:
+        print('|IceCream|')
+    elif i == 8:
+        print('|Price 23|')
+    else:
+        print('|        |')
